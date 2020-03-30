7d63034166d6fd8ca0ea2871c3f4b62c93033050
JoseAngulo <alphametaghost@gmail.com>
2020-03-30 11:33:20 +0100

    ,

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -7,8 +7,9 @@ function scrollToTop(){
 
 	});
 
-	let building_info_container = document.getElementById("scroll-arrow-button");
+	let building_info_container = document.getElementById("building-bg");
 	building_info_container.classList.add("d-none");
+	building_info_container.classList.remove("d-flex");
 
 }
 
