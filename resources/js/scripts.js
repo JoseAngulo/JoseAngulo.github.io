06863fdd98365ae1513eef887feb982be00f1091
JoseAngulo <alphametaghost@gmail.com>
2020-03-30 12:11:09 +0100

    Added timeOut function to hide building description after some time

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -7,9 +7,11 @@ function scrollToTop(){
 
 	});
 
-	let building_info_container = document.getElementById("building_bg");
-	building_info_container.classList.add("d-none");
-	building_info_container.classList.remove("d-flex");
+	setTimeout(function(){
+		let building_info_container = document.getElementById("building_bg");
+		building_info_container.classList.add("d-none");
+		building_info_container.classList.remove("d-flex");
+	}, 500);
 
 }
 
