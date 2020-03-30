d65a08dcf3a65152a14c9041a6dbdf5df0907c2b
JoseAngulo <alphametaghost@gmail.com>
2020-03-30 11:55:07 +0100

    JS updated to new building info container id

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -7,7 +7,7 @@ function scrollToTop(){
 
 	});
 
-	let building_info_container = document.getElementById("building-bg");
+	let building_info_container = document.getElementById("building_bg");
 	building_info_container.classList.add("d-none");
 	building_info_container.classList.remove("d-flex");
 
