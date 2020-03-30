698410fd5f25e0022a276850062d9b4a9af18832
JoseAngulo <alphametaghost@gmail.com>
2020-03-30 11:12:45 +0100

    Container related to building description now disappears instanty after clicking the scroll button

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,5 +1,15 @@
 
+function scrollToTop(){
+	let gameContainer = document.getElementById("gameContainer");
 
-//alert("Capa pulsada");
+	gameContainer.scrollIntoView({
+		behavior: 'smooth', block:'center', 
+
+	});
+
+	let building_info_container = document.getElementById("scroll-arrow-button");
+	building_info_container.classList.add("display_none");
+
+}
 
 
