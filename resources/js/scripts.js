02c05b26e0cf170ad405cfed4ec9429e8c5cae4d
JoseAngulo <alphametaghost@gmail.com>
2020-04-16 10:27:35 +0100

    HTML template modified to rescale the gameContainer when needed.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,17 +1,27 @@
 
 function scrollToTop(){
 	let gameContainer = document.getElementById("gameContainer");
+    gameContainer.classList.remove("d-none");
+
+    gameContainer.addEventListener("transitionend", hideBuildingBackground, false);
+    gameContainer.classList.remove('scaleDownGameContainer');
+    gameContainer.classList.add('scaleUpGameContainer');
 
 	gameContainer.scrollIntoView({
-		behavior: 'smooth', block:'center', 
+		behavior: 'smooth', block:'start', 
 
 	});
 
-	setTimeout(function(){
-		let building_info_container = document.getElementById("building_bg");
-		building_info_container.classList.add("d-none");
-	}, 500);
+}
 
+function hideBuildingBackground(){
+
+    document.getElementById("building_bg").classList.add("d-none");
+
+    document.getElementById("gameContainer").scrollIntoView({
+        behavior: 'smooth', block:'start', 
+
+    });
 }
 
 
