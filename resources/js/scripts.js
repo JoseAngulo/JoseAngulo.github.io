34a3c8c012f36caa32fb59d8837329f95d65dd9d
JoseAngulo <alphametaghost@gmail.com>
2020-04-21 14:47:02 +0100

    Unity player container is now scaled to full browser width and height. Floating arrow to scroll back to Unity player has been added. Layer button switching system is now implemented in a similar way to map switching system. Footer is now shown when a building description is loaded.

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,27 +1,33 @@
 
 function scrollToTop(){
 	let gameContainer = document.getElementById("gameContainer");
-    gameContainer.classList.remove("d-none");
 
+    gameContainer.removeEventListener("transitionend", showFixedArrowButton, false);
     gameContainer.addEventListener("transitionend", hideBuildingBackground, false);
+
+    gameContainer.scrollIntoView({
+        behavior: 'smooth', block:'start', 
+
+    });
+
+    document.getElementById("fixedArrowButton").classList.add("d-none");
+    document.getElementById("footer").classList.add("d-none");
+    gameContainer.classList.remove("d-none");
     gameContainer.classList.remove('scaleDownGameContainer');
     gameContainer.classList.add('scaleUpGameContainer');
-
-	gameContainer.scrollIntoView({
-		behavior: 'smooth', block:'start', 
-
-	});
 
 }
 
 function hideBuildingBackground(){
 
     document.getElementById("building_bg").classList.add("d-none");
+    
+}
 
-    document.getElementById("gameContainer").scrollIntoView({
-        behavior: 'smooth', block:'start', 
+function showFixedArrowButton(){
 
-    });
+    document.getElementById("fixedArrowButton").classList.remove("d-none");
+    
 }
 
 
