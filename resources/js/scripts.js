165f09ce0fa66f3bdb3eb82dea90d402c4ba28a6
JoseAngulo <alphametaghost@gmail.com>
2020-04-25 16:40:49 +0100

    Added new mobile main image

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,4 +1,20 @@
 
+window.addEventListener("DOMContentLoaded", hideUnityPlayer);
+
+
+function hideUnityPlayer(){
+
+    if(isMobileDevice()){
+        document.getElementById("loading-image").src = "resources/images/mobile_main_image.png";
+        document.getElementById("gameContainer").remove();
+        document.getElementById("spinner_wrapper").remove();
+    }
+}
+
+function isMobileDevice() {
+    return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+};
+
 function scrollToTop(){
 	let gameMainWrapper = document.getElementById("gameMainWrapper");
 
@@ -29,6 +45,8 @@ function showFixedArrowButton(){
     document.getElementById("fixedArrowButton").classList.remove("d-none");
     
 }
+
+
 
 
 
